63c260e8d4dc767cb692f0657af7c62089a973a1
YuDu-ankupanku <ankupanku19@gmail.com>
2025-05-06 23:00:15 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/watchparty-db');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ankupanku19:Uzwo9phoipAqm5GP@watchparty.aspdn1w.mongodb.net/okay');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
